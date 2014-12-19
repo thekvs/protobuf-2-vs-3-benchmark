be17d2bf810bf99d5a283e86f72c5c47bb3741de
Konstantin Sorokin <kvs@sigterm.ru>
2014-12-19 11:54:11 +0300

    added protobuf v.3 serialization test

---
+++ benchmark_v3.cpp
@@ -0,0 +1,103 @@
+#include <string>
+#include <set>
+#include <iostream>
+#include <stdexcept>
+#include <memory>
+#include <chrono>
+#include <sstream>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/lexical_cast.hpp>
+
+#include "generated/message/v3/message.pb.h"
+#include "data.hpp"
+
+void
+protobuf_serialization_test(size_t iterations)
+{
+    using namespace protobuf_v3_benchmark;
+
+    Record r1;
+
+    for (size_t i = 0; i < kIntegers.size(); i++) {
+        r1.add_i(kIntegers[i]);
+    }
+
+    for (size_t i = 0; i < kStringsCount; i++) {
+        r1.add_s(kStringValue);
+    }
+
+    for (size_t i = 0; i < kSmallIntegers.size(); i++) {
+        r1.add_small_i(kSmallIntegers[i]);
+    }
+
+    for (size_t i = 0; i < kDoubles1.size(); i++) {
+        r1.add_d1(kDoubles1[i]);
+    }
+
+    for (size_t i = 0; i < kDoubles2.size(); i++) {
+        r1.add_d2(kDoubles2[i]);
+    }
+
+    std::string serialized;
+
+    r1.SerializeToString(&serialized);
+
+    // check if we can deserialize back
+    Record r2;
+    bool ok = r2.ParseFromString(serialized);
+    if (!ok /*|| r2 != r1*/) {
+        throw std::logic_error("protobuf's case: deserialization failed");
+    }
+
+    std::cout << "protobuf: version = " << GOOGLE_PROTOBUF_VERSION << std::endl;
+    std::cout << "protobuf: size = " << serialized.size() << " bytes" << std::endl;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < iterations; i++) {
+        serialized.clear();
+        r1.SerializeToString(&serialized);
+        r2.ParseFromString(serialized);
+    }
+    auto finish = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
+
+    std::cout << "protobuf: time = " << duration << " milliseconds" << std::endl << std::endl;
+}
+
+int
+main(int argc, char **argv)
+{
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    if (argc < 2) {
+        std::cout << "usage: " << argv[0] << " N";
+        std::cout << std::endl << std::endl;
+        std::cout << "arguments: " << std::endl;
+        std::cout << " N  -- number of iterations" << std::endl << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+    size_t iterations;
+
+    try {
+        iterations = boost::lexical_cast<size_t>(argv[1]);
+    } catch (std::exception &exc) {
+        std::cerr << "Error: " << exc.what() << std::endl;
+        std::cerr << "First positional argument must be an integer." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "performing " << iterations << " iterations" << std::endl << std::endl;
+
+    try {
+        protobuf_serialization_test(iterations);
+    } catch (std::exception &exc) {
+        std::cerr << "Error: " << exc.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    google::protobuf::ShutdownProtobufLibrary();
+
+    return EXIT_SUCCESS;
+}
